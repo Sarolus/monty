56673e9de64d7c03332d29677f1e340b6a0a1ced
maba-dev <cherif.ba@hotmail.fr>
2021-05-12 21:12:07 +0000

    correction the pop fonction

--- pop.c
+++ pop.c
@@ -8,11 +8,12 @@ void pop(stack_t **stack)
 {
 	stack_t *tmp = *stack;
 
-	if (tmp == NULL)
+	if (stack == NULL || tmp == NULL)
 		errorHandler(POP_EMPTY);
 
 	*stack = tmp->next;
 
 	if (*stack != NULL)
 		(*stack)->prev = NULL;
+	free(tmp);
 }

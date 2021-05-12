ee0fbb0dd53c27206fad2b30f647f78c787560b8
Sarolus <cactussius.primus@gmail.com>
2021-05-12 17:30:55 +0200

    Remove WRONG_ARGUMENT error case for now

--- parsingManager.c
+++ parsingManager.c
@@ -7,12 +7,9 @@
  */
 void parsingManager()
 {
-	char *saveptr, *separator, *tmp;
+	char *saveptr, *separator;
 
 	separator = "\n\t ";
-
-	tmp = malloc(sizeof(char));
-	tmp = NULL;
 
 	if (data->buffer == NULL)
 		errorHandler(ALLOCATION_FAILED);
@@ -23,13 +20,5 @@ void parsingManager()
 		errorHandler(INVALID_INSTRUCTION);
 
 	if (strcmp(data->functionName, "push") == 0)
-		tmp = _strtok(NULL, separator, &saveptr);
-
-	printf("%s\n", tmp);
-	if (isnumber(tmp) != 0 /* || (tmp[0] == '-' && isnumber((tmp + 1)) != 0) */)
-	{
-		errorHandler(WRONG_ARGUMENT);
-	}
-	
-	data->value = atoi(tmp);
+		data->value = atoi(_strtok(NULL, separator, &saveptr));
 }
